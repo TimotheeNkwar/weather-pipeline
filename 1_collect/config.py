f6e722e44c976402f36f92c5394569cd6c5bf493
Timotheenkwar <133169218+TimotheeNkwar@users.noreply.github.com>
2025-07-13 08:21:03 +0300

    update

--- 1_collect/config.py
+++ 1_collect/config.py
@@ -4,7 +4,7 @@ API_KEY = "83b8fe86a826201d9399b34b21885224"
 
 CITIES = ["Paris", "Istanbul", "Lagos", "Nairobi", "New York"]
 
-MONGO_URI = "mongodb+srv://timotheenkwar16:22205731@pipeline.abcde.mongodb.net/?retryWrites=true&w=majority"
+MONGO_URI = "mongodb+srv://timotheenkwar16:22205731@pipeline.v8oxp4b.mongodb.net/?retryWrites=true&w=majority&appName=pipeline"
 MONGO_DB = "weatherDB"
 MONGO_COLLECTION = "raw_weather"
 
